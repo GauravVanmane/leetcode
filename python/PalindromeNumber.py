e4827092df311323dafce046303c09d9188bcba5
Gaurav <75316928+GauravVanmane@users.noreply.github.com>
2021-11-24 14:16:22 -0500

    Create PalindromeNumber.py

---
+++ python/PalindromeNumber.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x< 0:
+            return False
+        reversed_int = 0
+        y= x
+        while x:
+            reversed_int = reversed_int * 10 + x % 10
+            x //= 10
+        if reversed_int >= 2 ** 31 - 1 or reversed_int <= -2 ** 31:
+            return False
+        return True if reversed_int ==y else False
