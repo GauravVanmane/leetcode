2c0f6158a5b70c15eb3b02d8486820b0bb4feb36
Gaurav <75316928+GauravVanmane@users.noreply.github.com>
2021-11-29 12:56:58 -0500

    updated with comments single number

--- python/SingleNumber.py
+++ python/SingleNumber.py
@@ -2,7 +2,9 @@ def singleNumber(self, nums: List[int]) -> int:
     num = set()
     for n in nums:
         if n not in num:
+            #adding the element in the set
             num.add(n)
         else:
+            #removing the duplicate element
             num.remove(n)
     return list(num)[0]
